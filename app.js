b12ac2368b303adb7b0ac3024755a10e58563c3f
lezgin-1 <marat-memmed.18@gmail.com>
2022-04-22 16:47:01 +0400

    added

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const ikon = document.querySelector(".ikon");
 const navv = document.querySelector(".navv");
-const fa = document.querySelector(".fa-solid")
+const fa = document.querySelector(".fa-solid");
+const remo = document.querySelector(".ikon-2");
+const delet = document.querySelector(".fa-circle-minus")
 
 ikon.addEventListener("click", navbar);
+delet.addEventListener("click", remove);
 
 function navbar(){
         navv.style.left = "0";
-        fa.style.color = "#fff"
+        fa.style.display = "none";
+        remo.style.display = "block";
 }
+function remove(){
+        navv.style.left = "-100%";
+        fa.style.display = "block";
+        remo.style.display = "none";
+}
+
